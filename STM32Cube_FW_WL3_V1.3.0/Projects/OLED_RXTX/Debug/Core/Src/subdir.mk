54d3e37e05e073ffc0c9e2b628aa85e7b403c919
Alzbeta Oravcova (PGR) <a.oravcova.1@research.gla.ac.uk>
2026-02-09 12:34:15 +0000

    Add OLED project into the git folder

---
+++ STM32Cube_FW_WL3_V1.3.0/Projects/OLED_RXTX/Debug/Core/Src/subdir.mk
@@ -0,0 +1,42 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (13.3.rel1)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Core/Src/main.c \
+../Core/Src/stm32wl3x_hal_msp.c \
+../Core/Src/stm32wl3x_it.c \
+../Core/Src/syscalls.c \
+../Core/Src/sysmem.c \
+../Core/Src/system_stm32wl3x.c 
+
+OBJS += \
+./Core/Src/main.o \
+./Core/Src/stm32wl3x_hal_msp.o \
+./Core/Src/stm32wl3x_it.o \
+./Core/Src/syscalls.o \
+./Core/Src/sysmem.o \
+./Core/Src/system_stm32wl3x.o 
+
+C_DEPS += \
+./Core/Src/main.d \
+./Core/Src/stm32wl3x_hal_msp.d \
+./Core/Src/stm32wl3x_it.d \
+./Core/Src/syscalls.d \
+./Core/Src/sysmem.d \
+./Core/Src/system_stm32wl3x.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Core/Src/%.o Core/Src/%.su Core/Src/%.cyclo: ../Core/Src/%.c Core/Src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m0plus -std=gnu11 -g3 -DDEBUG -DUSE_NUCLEO_64 -DUSE_HAL_DRIVER -DSTM32WL3XX -c -I../Core/Inc -I"C:/Users/2578074O/Documents/STM32_Projects/OLEDcontrol/Drivers/OLED" -I../Drivers/STM32WL3x_HAL_Driver/Inc -I../Drivers/STM32WL3x_HAL_Driver/Inc/Legacy -I../Drivers/BSP/STM32WL3x-Nucleo -I../Drivers/CMSIS/Device/ST/STM32WL3X/Include -I../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
+clean: clean-Core-2f-Src
+
+clean-Core-2f-Src:
+	-$(RM) ./Core/Src/main.cyclo ./Core/Src/main.d ./Core/Src/main.o ./Core/Src/main.su ./Core/Src/stm32wl3x_hal_msp.cyclo ./Core/Src/stm32wl3x_hal_msp.d ./Core/Src/stm32wl3x_hal_msp.o ./Core/Src/stm32wl3x_hal_msp.su ./Core/Src/stm32wl3x_it.cyclo ./Core/Src/stm32wl3x_it.d ./Core/Src/stm32wl3x_it.o ./Core/Src/stm32wl3x_it.su ./Core/Src/syscalls.cyclo ./Core/Src/syscalls.d ./Core/Src/syscalls.o ./Core/Src/syscalls.su ./Core/Src/sysmem.cyclo ./Core/Src/sysmem.d ./Core/Src/sysmem.o ./Core/Src/sysmem.su ./Core/Src/system_stm32wl3x.cyclo ./Core/Src/system_stm32wl3x.d ./Core/Src/system_stm32wl3x.o ./Core/Src/system_stm32wl3x.su
+
+.PHONY: clean-Core-2f-Src
+
---
+++ STM32Cube_FW_WL3_V1.3.0/Projects/OLED_RXTX/Debug/Core/Startup/startup_stm32wl33ccvx.d
@@ -0,0 +1,2 @@
+Core/Startup/startup_stm32wl33ccvx.o: \
+ ../Core/Startup/startup_stm32wl33ccvx.s
