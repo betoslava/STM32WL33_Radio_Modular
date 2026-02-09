bb4738ee9cc8bbad6f964aad150f771ab0ed421c
Alzbeta Oravcova (PGR) <a.oravcova.1@research.gla.ac.uk>
2026-02-09 14:46:46 +0000

    setup OLED drivers

---
+++ STM32Cube_FW_WL3_V1.3.0/Projects/NUCLEO-WL33CC/Examples/GPIO/GPIO_EXTI/STM32CubeIDE/Debug/Drivers/STM32WL3x_HAL_Driver/subdir.mk
@@ -0,0 +1,82 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (13.3.rel1)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+C:/ST/stm32cubewl3-v1-3-0/STM32Cube_FW_WL3_V1.3.0/Drivers/STM32WL3x_HAL_Driver/Src/stm32wl3x_hal.c \
+C:/ST/stm32cubewl3-v1-3-0/STM32Cube_FW_WL3_V1.3.0/Drivers/STM32WL3x_HAL_Driver/Src/stm32wl3x_hal_cortex.c \
+C:/ST/stm32cubewl3-v1-3-0/STM32Cube_FW_WL3_V1.3.0/Drivers/STM32WL3x_HAL_Driver/Src/stm32wl3x_hal_dma.c \
+C:/ST/stm32cubewl3-v1-3-0/STM32Cube_FW_WL3_V1.3.0/Drivers/STM32WL3x_HAL_Driver/Src/stm32wl3x_hal_flash.c \
+C:/ST/stm32cubewl3-v1-3-0/STM32Cube_FW_WL3_V1.3.0/Drivers/STM32WL3x_HAL_Driver/Src/stm32wl3x_hal_flash_ex.c \
+C:/ST/stm32cubewl3-v1-3-0/STM32Cube_FW_WL3_V1.3.0/Drivers/STM32WL3x_HAL_Driver/Src/stm32wl3x_hal_gpio.c \
+C:/ST/stm32cubewl3-v1-3-0/STM32Cube_FW_WL3_V1.3.0/Drivers/STM32WL3x_HAL_Driver/Src/stm32wl3x_hal_pwr.c \
+C:/ST/stm32cubewl3-v1-3-0/STM32Cube_FW_WL3_V1.3.0/Drivers/STM32WL3x_HAL_Driver/Src/stm32wl3x_hal_pwr_ex.c \
+C:/ST/stm32cubewl3-v1-3-0/STM32Cube_FW_WL3_V1.3.0/Drivers/STM32WL3x_HAL_Driver/Src/stm32wl3x_hal_rcc.c \
+C:/ST/stm32cubewl3-v1-3-0/STM32Cube_FW_WL3_V1.3.0/Drivers/STM32WL3x_HAL_Driver/Src/stm32wl3x_hal_rcc_ex.c \
+C:/ST/stm32cubewl3-v1-3-0/STM32Cube_FW_WL3_V1.3.0/Drivers/STM32WL3x_HAL_Driver/Src/stm32wl3x_hal_tim.c \
+C:/ST/stm32cubewl3-v1-3-0/STM32Cube_FW_WL3_V1.3.0/Drivers/STM32WL3x_HAL_Driver/Src/stm32wl3x_hal_tim_ex.c 
+
+OBJS += \
+./Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal.o \
+./Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_cortex.o \
+./Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_dma.o \
+./Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_flash.o \
+./Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_flash_ex.o \
+./Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_gpio.o \
+./Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_pwr.o \
+./Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_pwr_ex.o \
+./Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_rcc.o \
+./Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_rcc_ex.o \
+./Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_tim.o \
+./Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_tim_ex.o 
+
+C_DEPS += \
+./Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal.d \
+./Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_cortex.d \
+./Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_dma.d \
+./Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_flash.d \
+./Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_flash_ex.d \
+./Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_gpio.d \
+./Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_pwr.d \
+./Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_pwr_ex.d \
+./Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_rcc.d \
+./Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_rcc_ex.d \
+./Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_tim.d \
+./Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_tim_ex.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal.o: C:/ST/stm32cubewl3-v1-3-0/STM32Cube_FW_WL3_V1.3.0/Drivers/STM32WL3x_HAL_Driver/Src/stm32wl3x_hal.c Drivers/STM32WL3x_HAL_Driver/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m0plus -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32WL3XX -c -I../../Inc -I../../../../../../../Drivers/STM32WL3x_HAL_Driver/Inc -I../../../../../../../Drivers/STM32WL3x_HAL_Driver/Inc/Legacy -I../../../../../../../Drivers/CMSIS/Device/ST/STM32WL3X/Include -I../../../../../../../Drivers/CMSIS/Include -I../../../../../../../Drivers/BSP/STM32WL3x-Nucleo -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_cortex.o: C:/ST/stm32cubewl3-v1-3-0/STM32Cube_FW_WL3_V1.3.0/Drivers/STM32WL3x_HAL_Driver/Src/stm32wl3x_hal_cortex.c Drivers/STM32WL3x_HAL_Driver/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m0plus -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32WL3XX -c -I../../Inc -I../../../../../../../Drivers/STM32WL3x_HAL_Driver/Inc -I../../../../../../../Drivers/STM32WL3x_HAL_Driver/Inc/Legacy -I../../../../../../../Drivers/CMSIS/Device/ST/STM32WL3X/Include -I../../../../../../../Drivers/CMSIS/Include -I../../../../../../../Drivers/BSP/STM32WL3x-Nucleo -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_dma.o: C:/ST/stm32cubewl3-v1-3-0/STM32Cube_FW_WL3_V1.3.0/Drivers/STM32WL3x_HAL_Driver/Src/stm32wl3x_hal_dma.c Drivers/STM32WL3x_HAL_Driver/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m0plus -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32WL3XX -c -I../../Inc -I../../../../../../../Drivers/STM32WL3x_HAL_Driver/Inc -I../../../../../../../Drivers/STM32WL3x_HAL_Driver/Inc/Legacy -I../../../../../../../Drivers/CMSIS/Device/ST/STM32WL3X/Include -I../../../../../../../Drivers/CMSIS/Include -I../../../../../../../Drivers/BSP/STM32WL3x-Nucleo -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_flash.o: C:/ST/stm32cubewl3-v1-3-0/STM32Cube_FW_WL3_V1.3.0/Drivers/STM32WL3x_HAL_Driver/Src/stm32wl3x_hal_flash.c Drivers/STM32WL3x_HAL_Driver/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m0plus -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32WL3XX -c -I../../Inc -I../../../../../../../Drivers/STM32WL3x_HAL_Driver/Inc -I../../../../../../../Drivers/STM32WL3x_HAL_Driver/Inc/Legacy -I../../../../../../../Drivers/CMSIS/Device/ST/STM32WL3X/Include -I../../../../../../../Drivers/CMSIS/Include -I../../../../../../../Drivers/BSP/STM32WL3x-Nucleo -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_flash_ex.o: C:/ST/stm32cubewl3-v1-3-0/STM32Cube_FW_WL3_V1.3.0/Drivers/STM32WL3x_HAL_Driver/Src/stm32wl3x_hal_flash_ex.c Drivers/STM32WL3x_HAL_Driver/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m0plus -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32WL3XX -c -I../../Inc -I../../../../../../../Drivers/STM32WL3x_HAL_Driver/Inc -I../../../../../../../Drivers/STM32WL3x_HAL_Driver/Inc/Legacy -I../../../../../../../Drivers/CMSIS/Device/ST/STM32WL3X/Include -I../../../../../../../Drivers/CMSIS/Include -I../../../../../../../Drivers/BSP/STM32WL3x-Nucleo -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_gpio.o: C:/ST/stm32cubewl3-v1-3-0/STM32Cube_FW_WL3_V1.3.0/Drivers/STM32WL3x_HAL_Driver/Src/stm32wl3x_hal_gpio.c Drivers/STM32WL3x_HAL_Driver/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m0plus -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32WL3XX -c -I../../Inc -I../../../../../../../Drivers/STM32WL3x_HAL_Driver/Inc -I../../../../../../../Drivers/STM32WL3x_HAL_Driver/Inc/Legacy -I../../../../../../../Drivers/CMSIS/Device/ST/STM32WL3X/Include -I../../../../../../../Drivers/CMSIS/Include -I../../../../../../../Drivers/BSP/STM32WL3x-Nucleo -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_pwr.o: C:/ST/stm32cubewl3-v1-3-0/STM32Cube_FW_WL3_V1.3.0/Drivers/STM32WL3x_HAL_Driver/Src/stm32wl3x_hal_pwr.c Drivers/STM32WL3x_HAL_Driver/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m0plus -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32WL3XX -c -I../../Inc -I../../../../../../../Drivers/STM32WL3x_HAL_Driver/Inc -I../../../../../../../Drivers/STM32WL3x_HAL_Driver/Inc/Legacy -I../../../../../../../Drivers/CMSIS/Device/ST/STM32WL3X/Include -I../../../../../../../Drivers/CMSIS/Include -I../../../../../../../Drivers/BSP/STM32WL3x-Nucleo -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_pwr_ex.o: C:/ST/stm32cubewl3-v1-3-0/STM32Cube_FW_WL3_V1.3.0/Drivers/STM32WL3x_HAL_Driver/Src/stm32wl3x_hal_pwr_ex.c Drivers/STM32WL3x_HAL_Driver/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m0plus -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32WL3XX -c -I../../Inc -I../../../../../../../Drivers/STM32WL3x_HAL_Driver/Inc -I../../../../../../../Drivers/STM32WL3x_HAL_Driver/Inc/Legacy -I../../../../../../../Drivers/CMSIS/Device/ST/STM32WL3X/Include -I../../../../../../../Drivers/CMSIS/Include -I../../../../../../../Drivers/BSP/STM32WL3x-Nucleo -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_rcc.o: C:/ST/stm32cubewl3-v1-3-0/STM32Cube_FW_WL3_V1.3.0/Drivers/STM32WL3x_HAL_Driver/Src/stm32wl3x_hal_rcc.c Drivers/STM32WL3x_HAL_Driver/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m0plus -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32WL3XX -c -I../../Inc -I../../../../../../../Drivers/STM32WL3x_HAL_Driver/Inc -I../../../../../../../Drivers/STM32WL3x_HAL_Driver/Inc/Legacy -I../../../../../../../Drivers/CMSIS/Device/ST/STM32WL3X/Include -I../../../../../../../Drivers/CMSIS/Include -I../../../../../../../Drivers/BSP/STM32WL3x-Nucleo -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_rcc_ex.o: C:/ST/stm32cubewl3-v1-3-0/STM32Cube_FW_WL3_V1.3.0/Drivers/STM32WL3x_HAL_Driver/Src/stm32wl3x_hal_rcc_ex.c Drivers/STM32WL3x_HAL_Driver/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m0plus -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32WL3XX -c -I../../Inc -I../../../../../../../Drivers/STM32WL3x_HAL_Driver/Inc -I../../../../../../../Drivers/STM32WL3x_HAL_Driver/Inc/Legacy -I../../../../../../../Drivers/CMSIS/Device/ST/STM32WL3X/Include -I../../../../../../../Drivers/CMSIS/Include -I../../../../../../../Drivers/BSP/STM32WL3x-Nucleo -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_tim.o: C:/ST/stm32cubewl3-v1-3-0/STM32Cube_FW_WL3_V1.3.0/Drivers/STM32WL3x_HAL_Driver/Src/stm32wl3x_hal_tim.c Drivers/STM32WL3x_HAL_Driver/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m0plus -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32WL3XX -c -I../../Inc -I../../../../../../../Drivers/STM32WL3x_HAL_Driver/Inc -I../../../../../../../Drivers/STM32WL3x_HAL_Driver/Inc/Legacy -I../../../../../../../Drivers/CMSIS/Device/ST/STM32WL3X/Include -I../../../../../../../Drivers/CMSIS/Include -I../../../../../../../Drivers/BSP/STM32WL3x-Nucleo -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_tim_ex.o: C:/ST/stm32cubewl3-v1-3-0/STM32Cube_FW_WL3_V1.3.0/Drivers/STM32WL3x_HAL_Driver/Src/stm32wl3x_hal_tim_ex.c Drivers/STM32WL3x_HAL_Driver/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m0plus -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32WL3XX -c -I../../Inc -I../../../../../../../Drivers/STM32WL3x_HAL_Driver/Inc -I../../../../../../../Drivers/STM32WL3x_HAL_Driver/Inc/Legacy -I../../../../../../../Drivers/CMSIS/Device/ST/STM32WL3X/Include -I../../../../../../../Drivers/CMSIS/Include -I../../../../../../../Drivers/BSP/STM32WL3x-Nucleo -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
+clean: clean-Drivers-2f-STM32WL3x_HAL_Driver
+
+clean-Drivers-2f-STM32WL3x_HAL_Driver:
+	-$(RM) ./Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal.cyclo ./Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal.d ./Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal.o ./Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal.su ./Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_cortex.cyclo ./Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_cortex.d ./Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_cortex.o ./Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_cortex.su ./Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_dma.cyclo ./Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_dma.d ./Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_dma.o ./Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_dma.su ./Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_flash.cyclo ./Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_flash.d ./Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_flash.o ./Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_flash.su ./Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_flash_ex.cyclo ./Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_flash_ex.d ./Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_flash_ex.o ./Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_flash_ex.su ./Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_gpio.cyclo ./Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_gpio.d ./Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_gpio.o ./Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_gpio.su ./Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_pwr.cyclo ./Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_pwr.d ./Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_pwr.o ./Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_pwr.su ./Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_pwr_ex.cyclo ./Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_pwr_ex.d ./Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_pwr_ex.o ./Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_pwr_ex.su ./Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_rcc.cyclo ./Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_rcc.d ./Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_rcc.o ./Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_rcc.su ./Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_rcc_ex.cyclo ./Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_rcc_ex.d ./Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_rcc_ex.o ./Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_rcc_ex.su ./Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_tim.cyclo ./Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_tim.d ./Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_tim.o ./Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_tim.su ./Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_tim_ex.cyclo ./Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_tim_ex.d ./Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_tim_ex.o ./Drivers/STM32WL3x_HAL_Driver/stm32wl3x_hal_tim_ex.su
+
+.PHONY: clean-Drivers-2f-STM32WL3x_HAL_Driver
+
--- STM32Cube_FW_WL3_V1.3.0/Projects/OLED_RXTX/Debug/Core/Startup/startup_stm32wl33ccvx.d
+++
@@ -1,2 +0,0 @@
-Core/Startup/startup_stm32wl33ccvx.o: \
- ../Core/Startup/startup_stm32wl33ccvx.s
